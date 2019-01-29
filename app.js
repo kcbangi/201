5b0e7c37e8b386884690a0db1946783b6ddd5346
Keith <kayzee@live.com>
2019-01-28 21:24:46 -0800

    Update#2 README.md

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var dogs = prompt('How many dogs do I own');
+
+console.log('Numbers of dogs', dogs);
+
+alert('Number of dogs, ', + dogs)
+
+var guitars = prompt('How many guitars do you have?');
+
+if (guitar === '1' || guitars === '2') {
+    alert('Yes I have ' + guitar + 'guitars!');
+}
